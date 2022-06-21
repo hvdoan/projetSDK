67cfc21ba6b7975d962ea5ade6450aa6c3d09f2f
Armand DFL <armanddfl@gmail.com>
2022-06-21 15:35:16 +0200

    enh: add Discord provider

--- client/index.php
+++ client/index.php
@@ -4,6 +4,8 @@ define('OAUTH_CLIENT_ID', '621f59c71bc35');
 define('OAUTH_CLIENT_SECRET', '621f59c71bc36');
 define('FACEBOOK_CLIENT_ID', '1311135729390173');
 define('FACEBOOK_CLIENT_SECRET', 'fc5e25661fe961ab85d130779357541e');
+define('DISCORD_CLIENT_ID', '988786626991894548');
+define('DISCORD_CLIENT_SECRET', 'fiJS3nTn5cT0EymCPLFrGGXg3ZS8ok9Y');
 
 function login()
 {
@@ -30,6 +32,15 @@ function login()
         "state" => bin2hex(random_bytes(16))
     ]);
     echo "<a href=\"https://www.facebook.com/v2.10/dialog/oauth?{$queryParams}\">Login with Facebook</a>";
+
+    $queryParams= http_build_query([
+        'client_id' => DISCORD_CLIENT_ID,
+        'redirect_uri' => 'http://localhost:8081/ds_callback',
+        'response_type' => 'code',
+        'scope' => 'identify email',
+        "state" => bin2hex(random_bytes(16))
+    ]);
+    echo "<a href=\"https://discord.com/api/oauth2/authorize?{$queryParams}\">Login with Disocrd</a>";
 }
 
 // Exchange code for token then get user info
@@ -96,6 +107,40 @@ function fbcallback()
     echo "Hello {$user['name']}";
 }
 
+function dscallback()
+{
+    ["code" => $code, "state" => $state] = $_GET;
+
+    $specifParams = [
+        'client_id' => DISCORD_CLIENT_ID,
+        'client_secret' => DISCORD_CLIENT_SECRET,
+        'grant_type' => 'authorization_code',
+        'code' => $code,
+        'redirect_uri' => 'http://localhost:8081/ds_callback',
+    ];
+
+    $context = stream_context_create([
+        'http' => [
+            'header' => "Content-Type: application/x-www-form-urlencoded",
+            'method' => "POST",
+            'content' => http_build_query($specifParams)
+        ]
+    ]);
+
+    $result = file_get_contents('https://discord.com/api/oauth2/token', false, $context);
+    $token = json_decode($result, true);
+
+    $context = stream_context_create([
+        'http' => [
+            'header' => "Authorization: Bearer {$token['access_token']}"
+        ]
+    ]);
+
+    $response = file_get_contents("https://discord.com/api/users/@me", false, $context);
+
+    var_dump($response);
+}
+
 $route = $_SERVER["REQUEST_URI"];
 switch (strtok($route, "?")) {
     case '/login':
@@ -106,6 +151,9 @@ switch (strtok($route, "?")) {
         break;
     case '/fb_callback':
         fbcallback();
+        break;
+    case '/ds_callback':
+        dscallback();
         break;
     default:
         http_response_code(404);
